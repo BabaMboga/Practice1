4b71dc74c873e00b7a38404d607664aa925be1b4
AyimWilliam <odhisayim@gmail.com>
2023-03-14 11:31:12 +0300

    An example of an object

---
+++ index.js
@@ -0,0 +1,18 @@
+// This is an example of an object
+const js = {
+    name : "javascript",
+    createdBy : {
+        FirstName : "Brandon",
+        LastName : "Stark",
+    },
+    firstReleased : 1995,
+    isAwesome: true,
+    placeManufactured :{
+        Location : "Winterfell",
+        House : "Bolton",
+    }
+
+}
+
+typeof {};
+// the objeccts properties have four data types that include a string, boolean, an object and a number
